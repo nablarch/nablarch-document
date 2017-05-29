fc1b60249eade6ab1170cf8d878fa4353f90df4d
MAENO Daisuke <maeno.daisuke@tis.co.jp>
2017-05-29 09:56:40 +0900

    Google検索URLのLATEST化対応

--- _static/custom.js
+++ _static/custom.js
@@ -3,7 +3,7 @@ $(function () {
   var version = $('#document-version').val();
   var q = $('#q');
   var text = $('#text');
-  var url = 'nablarch.github.io/docs/' + version + '/doc';
+  var url = 'nablarch.github.io/docs/LATEST/doc';
   form.submit(function (event) {
     q.val('site:' + url + ' ' + text.val());
   });
